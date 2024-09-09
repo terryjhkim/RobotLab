37d85ecbf15ce7244f50cbebda21143f241ae32d
terryjhkim <155316859+terryjhkim@users.noreply.github.com>
2024-09-09 17:23:56 -0400

    Update RobotPong.py

--- RobotPong.py
+++ RobotPong.py
@@ -1,82 +1,62 @@
 from irobot_edu_sdk.backend.bluetooth import Bluetooth
-from irobot_edu_sdk.robots import event, hand_over, Color, Robot, Root, Create3
+from irobot_edu_sdk.robots import event, Robot, Color, Create3
 from irobot_edu_sdk.music import Note
 
-import math as m
+import math
 
-
-# robot is the instance of the robot that will allow us to call
-# its methods and to define events with the @event decorator.
 robot = Create3(Bluetooth("M3GAN"))
 
+def calculateApproachAngle(sensor_data, angles_list):
+    """Calculate the angle of the closest object based on sensor data."""
+    min_value = min(sensor_data)
+    min_index = sensor_data.index(min_value)
+    return angles_list[min_index]
 
-def getApproachAngle(readings, angles):
-    minReading = readings[0]
-    proximityAngle = angles[0]
-    for i in range(len(readings)):
-        sensorReading = readings[i]
-        if sensorReading < minReading:
-            minReading = sensorReading
-            closestAngle = angles[i]
-            proximityAngle = angles[i]
-    return proximityAngle
+@event(robot.when_touched, [True, False]) 
+async def handle_left_button(robot):
+    await robot.move(0) 
+    await robot.set_lights_rgb(255, 0, 0)  
+    
+@event(robot.when_touched, [False, True]) 
+async def handle_right_button(robot):
+    await robot.move(0) 
+    await robot.set_lights_rgb(255, 0, 0) 
 
-# LEFT BUTTON
-@event(robot.when_touched, [True, False])  # User buttons: [(.), (..)]
-async def when_left_button_touched(robot):
-    await robot.move(0)
-    await robot.set_lights_rgb(255, 0, 0)
-  
-
-
-# RIGHT BUTTON
-@event(robot.when_touched, [False, True])  # User buttons: [(.), (..)]
-async def when_right_button_touched(robot):
-    await robot.move(0)
-    await robot.set_lights_rgb(255, 0, 0)
-
-
-# EITHER BUMPER
-@event(robot.when_bumped, [True, True])  # [left, right]
-async def when_either_bumped(robot):
-    await robot.move(0)
-    await robot.set_lights_rgb(255, 0, 0)
- 
-
+@event(robot.when_bumped, [True, True]) 
+async def handle_bump(robot):
+    await robot.move(0) 
+    await robot.set_lights_rgb(255, 0, 0) 
 
 @event(robot.when_play)
-async def robotPong(robot):
-    await robot.set_lights(Robot.LIGHT_SPIN, Color(0,100,100))
-    await robot.set_wheel_speeds(15, 15)
-    
+async def play_pong(robot):
+    await robot.set_lights(Robot.LIGHT_SPIN, Color(0, 100, 100)) 
+    await robot.set_wheel_speeds(15, 15) 
+
     while True:
         angles = [-65.3, -38, -20, -3, 14.25, 34, 65.3]
-        ir_reading = (await robot.get_ir_proximity()).sensors
+        ir_proximity = (await robot.get_ir_proximity()).sensors
         light_color = "cyan"
 
-        for i in range(len(ir_reading)):
-            proximity = 4095/(ir_reading[i]+1)
-            print(proximity)
+        for reading in ir_proximity:
+            proximity_value = 4095 / (reading + 1)
+            print(proximity_value)
 
-            if proximity <= 20:
+            if proximity_value <= 20:
                 if light_color == "cyan":
-                    await robot.set_lights(Robot.LIGHT_SPIN, Color(255, 0, 255))
+                    await robot.set_lights(Robot.LIGHT_SPIN, Color(255, 0, 255))  # Change to magenta
                     light_color = "magenta"
-                elif light_color == "magenta":
-                    await robot.set_lights(Robot.LIGHT_SPIN, Color(0, 100, 100))
+                else:
+                    await robot.set_lights(Robot.LIGHT_SPIN, Color(0, 100, 100))  # Change to cyan
                     light_color = "cyan"
 
-                approachAngle = getApproachAngle(ir_reading, angles)
-                if approachAngle < 0:
-                    reflectionAngle = 180 + 2 * approachAngle
-                    await robot.turn_right(reflectionAngle)
-                    await robot.set_wheel_speeds(15,15)
+                approach_angle = calculateApproachAngle(ir_proximity, angles)
+                if approach_angle < 0:
+                    reflection_angle = 180 + 2 * approach_angle
+                    await robot.turn_right(reflection_angle)
                 else:
-                    reflectionAngle = 180 - 2 * approachAngle
-                    await robot.turn_left(reflectionAngle)
-                    await robot.set_wheel_speeds(15,15)
-    
+                    reflection_angle = 180 - 2 * approach_angle
+                    await robot.turn_left(reflection_angle)
 
-    
-# ask for desired
+                await robot.set_wheel_speeds(15, 15)
+
 robot.play()
